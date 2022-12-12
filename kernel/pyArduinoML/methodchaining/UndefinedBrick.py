6b127d182cf4a4fc8a069b58c33b6dc8d1b2f127
Amine-hub787 <71392330+Amine-hub787@users.noreply.github.com>
2022-12-12 09:38:10 +0100

    delete old kernel

--- kernel/pyArduinoML/methodchaining/UndefinedBrick.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'pascalpoizat'
-
-
-class UndefinedBrick(Exception):
-    """
-    Exception for references to undefined bricks.
-    """
--- kernel/pyArduinoML/methodchaining/UndefinedState.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'pascalpoizat'
-
-
-class UndefinedState(Exception):
-    """
-    Exception for references to undefined states
-    """
